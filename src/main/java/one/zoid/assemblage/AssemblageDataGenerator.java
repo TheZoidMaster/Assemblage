a85b1de8657ee6a9b5aaff256e40ede143721946
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-12-06 06:59:30 -0600

    guh

---
+++ src/main/java/one/zoid/assemblage/AssemblageDataGenerator.java
@@ -0,0 +1,11 @@
+package one.zoid.assemblage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AssemblageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
